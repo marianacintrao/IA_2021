c690e382398b3e3ae84db909d7b4a2385d490eca
mariana cintrao <mariana.cintrao@gmail.com>
2020-12-02 15:03:40 +0000

    tree kind of working

--- P2/IA2021proj2alunos/solutions/al098.py
+++ P2/IA2021proj2alunos/solutions/al098.py
@@ -8,9 +8,6 @@ Student id #93737
 from collections import Counter
 import numpy as np
 
-incertezas = []
-GI = []
-
 def incerteza(p, n):
     ''' returns I( p / p+n, n / p+n )  '''
     if p == 0 or n == 0:
@@ -21,7 +18,8 @@ def incerteza(p, n):
     xlog = np.log2(x)
     return (-P * xlog[0]) - (N * xlog[1])
 
-def createTreeAux(D,Y, noise = False):
+def createTreeAux(D, Y, noise = False, f_index = 0):
+    
     total = len(Y)
     n = Y.count(0)
     p = total - n
@@ -29,9 +27,7 @@ def createTreeAux(D,Y, noise = False):
 
     features = len(D[0])  
 
-    global incertezas
     incertezas = []
-    global GI
     GI = []
 
     for feature in range(0, features):
@@ -39,15 +35,11 @@ def createTreeAux(D,Y, noise = False):
         resto = 0
         incerteza_feature = {}
 
-        #feature Y list is a list of pairs [feature instance, Y instance] 
         feature_Y_list = [] 
-        #feature elements is a set of all unique feature elements
-        feature_elements = set() 
         for i in range(0, total):
             feature_Y_list += [[D[i][feature], Y[i]]]
-            feature_elements.add(D[i][feature])
 
-        for e in feature_elements:
+        for e in [0, 1]:
             p_instances = feature_Y_list.count([e, 1])
             n_instances = feature_Y_list.count([e, 0])
             I = incerteza(p_instances, n_instances)
@@ -57,39 +49,82 @@ def createTreeAux(D,Y, noise = False):
         GI += [initial_entropy - resto]
         incertezas += [incerteza_feature]
 
-    # feature com maior valor de GI
-    max_f = GI.index(max(GI))
-    return 
+    # print(GI, incertezas)
 
+    # feature com maior valor de GI
+    feature_index = GI.index(max(GI))
+    if max(GI) == 0:
+        if feature_index == f_index:
+            feature_index += 1
+        L = [feature_index] 
+        D0 = []
+        D1 = []
+        Y0 = []
+        Y1 = []
+        for i in range(len(D)):
+            if D[i][feature_index] == 0:
+                Y0 += [Y[i]]
+                D0 += [D[i]]
+            else: # D[i][feature_index] == 1:
+                Y1 += [Y[i]]
+                D1 += [D[i]]
+
+        if D0 == []:
+            return [1]
+        else:
+            L += [createTreeAux(D0, Y0, noise, feature_index)] 
+        if D1 == []:
+            return [0]
+        else:               
+            L += [createTreeAux(D1, Y1, noise, feature_index)]  
+        return L              
+
+    if max(GI) == 1:
+        L = [feature_index]
+        for key in [0,1]:
+            for i in range(len(D)):
+                if D[i][feature_index] == key:
+                    L += [Y[i]]
+                    break
+        return L
+
+    L = [feature_index]
+    for key in [0,1]:
+        if incertezas[feature_index][key] != 0:
+            new_D = []
+            new_Y = []
+            for i in range(len(Y)):
+                if D[i][feature_index] == key:
+                    new_D += [D[i]]
+                    new_Y += [Y[i]]
+            
+            L += [createTreeAux(new_D, new_Y)]
+        else:
+            for i in range(len(D)):
+                if D[i][feature_index] == key:
+                    L += [Y[i]]
+                    break
+    return L
 
 def createdecisiontree(D,Y, noise = False):
-
-    Daux = D.copy()
-    Yaux = Y.copy()
-    
-    while(1):
-        createTreeAux(Daux, Yaux)
-        feature_index = GI.index(max(GI))
-        if max(GI) == 0 or all(value == 0 for value in incertezas[feature_index].values()):
-            break
-        for i in range(len(Yaux)):
-            if Daux[i][feature_index] != "++"
-
-
-
+    T = createTreeAux(D, Y)
+    print(T)
 
     return [0,0,1] # to remove
     # return T
 
 
-D = [['a', '+', 'n'], ['a', '++', 's'], ['m', '+', 'n'], ['a', '++', 'n'], ['b', '+++', 's'], ['m', '+++', 's'], ['a', '+++', 's'], ['b', '+', 's']]
-Y = [0, 1, 0, 0, 1, 1, 1, 0]
-createdecisiontree(D, Y)
-# createdecisiontree([[0,0],[0,1],[1,0],[1,1]], [0,0,0,1])
-# createdecisiontree([['s', 's', 'b'],['n', 'n', 'a'], ['s', 's', 'c'], ['s', 'n', 'c'], ['n', 's', 'b'], ['n', 'n', 'a'], ['s', 'n', 'c'], ['n', 'n', 'a']], [0, 1, 0, 0, 0, 1, 0, 0])
-D = [['a', '+', 'n'], ['a', '++', 's'], ['m', '+', 'n'], ['a', '++', 'n'], ['b', '+++', 's'], ['m', '+++', 's'], ['a', '+++', 's'], ['b', '+', 's']]
-Y = [0, 1, 0, 0, 1, 1, 1, 0]
+D = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]]
+Y = [0, 1, 1, 0, 0, 1, 1, 0]
 
+
+# # este afinal estava certo, passei foi o teste mal
+# D = [[0,0,0], [0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0], [1,1,1]]
+# Y = [0, 1, 0, 1, 0, 1, 0, 1]
+
+# D = [[0,0], [0,1], [1,0], [1,1]]
+# Y = [0, 0, 0, 1]
+# createdecisiontree(D, Y)
 
 
 
