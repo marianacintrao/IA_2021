a8d1fd0e1372ae088b5b92bef9a0bf15e56e4740
lucet15 <lucialucet@gmail.com>
2020-12-05 17:43:33 +0000

    9.2

--- P2/IA2021proj2alunos/testdecisiontrees.py
+++ P2/IA2021proj2alunos/testdecisiontrees.py
@@ -29,6 +29,7 @@ B  = '\033[34m' # blue
 P  = '\033[35m' # purple
 
 
+
 def classify(T,data):
     
     data = np.array(data)
@@ -39,13 +40,13 @@ def classify(T,data):
         for ii in range(len(el)):
             #print(T[0],el[T[0]],T)
             if el[wT[0]]==0:
-                if isinstance(wT[1],int):
+                if not isinstance(wT[1], list):
                     out += [wT[1]]
                     break
                 else:
                     wT = wT[1]
             else:
-                if isinstance(wT[2],int):
+                if not isinstance(wT[2], list):
                     out += [wT[2]]
                     break
                 else:
@@ -72,14 +73,11 @@ for file in files:
             points = 0
             pointsshort = 0
 
-            for idataset in range(0,23):
+            for idataset in range(0,27):     
                 D,Y,nl,ol = datasetstreelearning.dataset(idataset)        
                                     
                 print("<", idataset, "> #points >", D.shape[0], "#feat >", D.shape[1])
                 try:
-                    # print(D)
-                    # print(Y)
-                    # exit()
                     T = M.createdecisiontree(D,Y,noise=0)
                     Yp = classify(T,D)
                 
@@ -90,7 +88,7 @@ for file in files:
                         points += 1
                         if l>nl:
                             res = R+'TOO BIG (0)'+W
-                            points -= 1
+                            points -= 2
                         elif l<nl:
                             res = G+'GOOD SMALL TREE (1+bonus)'+W
                             pointsshort += 1
@@ -98,17 +96,15 @@ for file in files:
                             if ol<nl:
                                 res = O+'OK but it is possible to make it shorter! (1)'+W
                             else:
-                                res = G+'OK (1)'+W                                
+                                res = G+'OK (1)'+W
                     else:
                         res = R+'Erro (0)r'+W
                     print("    errors > ", err, "tree length", l, " ", res  )
                     #print("\nD", D, "\nY", Y)
                     #print("tree > ", T)
-                    # print("try2")
                 except:
-                    # print("ERRO1")
                     print(R+"Test failed")
-            print("points", points, "/23", "short", pointsshort, "/2")
+            print("points", points, "/26", "short", pointsshort, "/2")
             
             # DATASETS WITH NOISE
             print("\n\n\t Testing robustness to noise\n\n")
@@ -116,12 +112,12 @@ for file in files:
             points2 = 0
             pointsgen = 0
             for idataset in [0,1,2,3]:
+            #for idataset in []:
                 D,Y,Dt,Yt,nl,ol = datasetstreelearning.datasetnoise(idataset)        
                                     
                 print("dataset > ", idataset, "#points >", D.shape[0], "#feat >", D.shape[1])
                 #print("\nD", D, "\nY", Y)
                 try:
-                    print("try1")
                     Tc = M.createdecisiontree(D,Y,noise=0.1)
                     Ytrain = classify(Tc,D)
                     Ytest = classify(Tc,Dt)
@@ -140,7 +136,6 @@ for file in files:
                         pointsgen += 1
                     print("  error training > ", errtrain, "length", lc, " error test > ", errtest, "/", ol, "\n", res )
                 except:
-                    print("ERRO2")
                     print(R+"Test failed")            
             
             print("points", points2, "/4", "good in test", pointsgen, "/4")
@@ -148,11 +143,11 @@ for file in files:
             
             
             print(P+"\n\n\t Result\n\n"+W)
-            print("tree no noise", points, "/23", "short", pointsshort, "/2")
+            print("tree no noise", points, "/26", "short", pointsshort, "/2")
             print("tree noise", points2, "/4", "good in test", pointsgen, "/4")
-            print("points",round(points/23*8,1),"+ reduced tree ", round(pointsshort/2*3,1), "+ noise", round(pointsgen/4*3,1))
-            C.append([file,round(points/23*8,1),round(pointsshort/2*3,1),round(pointsgen/4*3,1)])
-            print("\nExpected grade:\t", round(points/23*8,1)+round(pointsshort/2*3,1)+round(pointsgen/4*3,1),"(/14)+hidden tests (4pt)+report (2pt)")
+            print("points",round(points/26*8,1),"+ reduced tree ", round(pointsshort/2*3,1), "+ noise", round(pointsgen/4*3,1))
+            C.append([file,round(points/26*8,1),round(pointsshort/2*3,1),round(pointsgen/4*3,1)])
+            print("\nExpected grade:\t", round(points/26*8,1)+round(pointsshort/2*3,1)+round(pointsgen/4*3,1),"(/14)+hidden tests (4pt)+report (2pt)")
     except:
             print(R+"Error loading file, or running!")
             
